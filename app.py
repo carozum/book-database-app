6e11cc5f656ebc14c9183a12d7376ff045e88016
carozum <czumbiehlfaure@hotmail.fr>
2023-09-21 20:48:38 +0200

    able to view all the books

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ A console program :
 
 """
 
-from models import (Base, session, 
+from models import (Base, session,
                     Book, engine)
 import csv
 import datetime
 import time
 
 
-
 def main_menu():
     while True:
-        
+
         print("""
             
             \nPROGRAMMING BOOKS
@@ -27,7 +26,7 @@ def main_menu():
         choice = input("What would you like to do ? ")
         if choice in ['1', '2', '3', '4', '5']:
             return choice
-        else :
+        else:
             choice = input("""
                         \rPlease choose one of the options above.
                         \rA number from 1 to 5.
@@ -35,9 +34,10 @@ def main_menu():
 
 
 def clean_date(date_str):
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    months = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
     split_date = date_str.split(' ')
-    
+
     try:
         month = months.index(split_date[0]) + 1
         day = int(split_date[1][:-1])
@@ -45,26 +45,26 @@ def clean_date(date_str):
         date = datetime.date(year, month, day)
     except ValueError:
         input('''
-              \n******* DATE ERROR *********
-              \rThe date format should include a valid Month Day, Year from the past. 
-              \rEx: January 13, 2003
-              \rPress enter to try again
-              \r************************''')
+            \n******* DATE ERROR *********
+            \rThe date format should include a valid Month Day, Year from the past. 
+            \rEx: January 13, 2003
+            \rPress enter to try again
+            \r************************''')
         return
-    else:  
+    else:
         return date
 
 
 def clean_price(price_str):
-    try: 
+    try:
         price_float = float(price_str)
     except ValueError:
         input('''
-              \n******* PRICE ERROR *****
-              \rThe price should be a number without a currency symbol. 
-              \rEx: 10.99
-              \rPress enter to try again
-              \r*********************''')
+            \n******* PRICE ERROR *****
+            \rThe price should be a number without a currency symbol. 
+            \rEx: 10.99
+            \rPress enter to try again
+            \r*********************''')
         return
     else:
         return int(price_float * 100)
@@ -75,16 +75,17 @@ def add_csv():
     with open('./suggested_books.csv') as csv_file:
         # Create reader object by passing the file object to reader method
         data = csv.reader(csv_file)
-        # Iterate over each row in the csv 
+        # Iterate over each row in the csv
         # file using reader object
         for row in data:
             # avoid duplicate books
-            book_in_db = session.query(Book).filter( Book.title == row[0]).one_or_none()
-            if book_in_db == None :
+            book_in_db = session.query(Book).filter(
+                Book.title == row[0]).one_or_none()
+            if book_in_db == None:
                 new_book = Book(
-                    title=row[0], 
-                    author=row[1], 
-                    published_date=clean_date(row[2]), 
+                    title=row[0],
+                    author=row[1],
+                    published_date=clean_date(row[2]),
                     price=clean_price(row[3]))
                 session.add(new_book)
         session.commit()
@@ -94,28 +95,34 @@ def add_book():
     print("Add a New Book")
     title = input("Book title: ")
     author = input("Author: ")
-    
+
     date_error = True
     while date_error:
         date = input("Published Date (Example: January 13, 2023): ")
         date = clean_date(date)
         if type(date) == datetime.date:
             date_error = False
-    
+
     price_error = True
     while price_error:
         price = clean_price(input("Price (Example: 10.99): "))
         if type(price) == int:
             price_error = False
-        
-    new_book = Book(title= title,
-                    author = author,
-                    published_date = date,
-                    price= price)
+
+    new_book = Book(title=title,
+                    author=author,
+                    published_date=date,
+                    price=price)
     session.add(new_book)
     session.commit()
     print("Book added!")
     time.sleep(1.5)
+
+
+def view_books():
+    for book in session.query(Book):
+        print(f"{book.id} | {book.title} | {book.author}")
+    input('\nPress enter to return to the main menu.')
 
 
 def app():
@@ -125,12 +132,10 @@ def app():
         match choice:
             case '1':
                 add_book()
-                pass
             case '2':
-                # search book
-                pass
+                view_books()
             case '3':
-                # view book
+                # search book
                 pass
             case '4':
                 # analysis
@@ -139,16 +144,14 @@ def app():
                 print("GOODBYE")
                 app_running = False
 
+
 if __name__ == "__main__":
-    Base.metadata.create_all(engine) 
+    Base.metadata.create_all(engine)
     add_csv()
     app()
-    
+
     for book in session.query(Book):
         print(book)
-    
-    
-
 
 
 # display the menu inside the search module
@@ -168,18 +171,13 @@ def search_menu():
             raise ValueError("You did not enter 1, 2 or 3")
     except ValueError as e:
         print("Enter 1, 2 or 3")
-    else: 
+    else:
         if choice == 1:
             edit_book()
-        if choice == 2 :
+        if choice == 2:
             delete_book()
-        if choice==3:
+        if choice == 3:
             main_menu()
-
-
-
-def all_books():
-    print("all_books function")
 
 
 # *************** function to edit a book
@@ -204,7 +202,7 @@ def search_book():
             raise ValueError("You have not entered a correct ID")
     except ValueError as e:
         print("You have to enter an integer")
-    else: 
+    else:
         print("book_title")
         print("Published: date_published ")
         print("Current Price: $ price")
@@ -218,4 +216,3 @@ def book_analysis():
     print("Total Number of Books: 14")
     print("Total Number of Python books: 8")
     main_menu()
-    
